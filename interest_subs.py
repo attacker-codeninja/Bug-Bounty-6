8584091bd386084a208840edf8dd78573104f908
Ahmed Taher <trnotfy4all@gmail.com>
2023-04-21 02:05:01 +0300

    Update interest_subs.py

--- interest_subs.py
+++ interest_subs.py
@@ -2,14 +2,15 @@ import sys
 
 #Author: Ahmed Taher (@IAmAhmedTahar)
 #Tool to find interesting subdomains
+#The tool read from stdin
 
 interesting_subs = ['admin','user','login','register','mail','api', 'blog', 'domain', 'test', 'proxy', 'stage', 'devops' , 'staff', 'db', 'internal', 'shop', 'news', 'landing']
 
 for line in sys.stdin:
     if 'Exit' == line.rstrip():
         break
-    host = line.partition('://')[2]
-    sub_addr = host.partition('.')[0]
+
+    sub_addr = line.partition('.')[0]
 
     if sub_addr in interesting_subs:
     	print(line.strip())
